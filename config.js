26ac4081004d7800504ea081aa62c23b7301a0ed
Daniel Falana <167510072+Paulschol@users.noreply.github.com>
2024-08-20 12:28:06 +0100

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i += 2) {
     global.owner.push(owner);
 }
 
-//global.pairingNumber = "" //put your bot number here
+//global.pairingNumber = "GuruBot~pt2Y67bK" //put your bot number here
 
 global.mods = ['917605902011', '32460248586']
 global.prems = ['917605902011', '32460248586', '919398758484']
